28fd8fcc89ada043e01ba1e33772f7ae05bbf1a3
ZakariaLagraini <zakarialaganimes@gamil.com>
2023-12-22 20:33:21 +0100

    livres affichages

--- Livres.xaml.cs
+++ Livres.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,11 +19,57 @@ namespace GestionBiblio
     /// <summary>
     /// Interaction logic for Livres.xaml
     /// </summary>
+    /// 
+        public class DataAccess
+        {
+            private readonly string connectionString;
+
+            public DataAccess(string connectionString)
+            {
+                this.connectionString = connectionString;
+            }
+
+            public DataTable GetBooksData()
+            {
+                DataTable dataTable = new DataTable();
+
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    try
+                    {
+                        connection.Open();
+                        string query = "SELECT Titre, Auteurs, AnneePublication, Genres, Etat FROM livres";
+                        MySqlCommand command = new MySqlCommand(query, connection);
+                        MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+                        adapter.Fill(dataTable);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+
+                return dataTable;
+            }
+        }
+    
+
     public partial class Livres : UserControl
     {
+        private readonly string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=zaratapa;database=dotnet;";
+        private readonly DataAccess dataAccess;
+
         public Livres()
         {
             InitializeComponent();
+            dataAccess = new DataAccess(connectionString);
+
+            BindDataToGrid();
+        }
+
+        private void BindDataToGrid()
+        {
+            dataGridBooks.ItemsSource = dataAccess.GetBooksData().DefaultView;
         }
     }
 }
